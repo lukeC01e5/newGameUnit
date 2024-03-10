e1be7026d73df6969066c55a6dbab8b6a42cdb7f
lukeC01e5 <colesluke@gmial.com>
2024-03-10 22:43:20 +1300

    tried to make it more ordered

---
+++ src/displayFunctions.h
@@ -0,0 +1,100 @@
+
+#include <TFT_eSPI.h> // Include the TFT_eSPI library
+
+#include "Trex.h" // Include the T-Rex image
+
+TFT_eSPI tft; // Create an instance of the TFT class
+
+void displayTrex()
+{
+  tft.fillScreen(TFT_BLACK); // Clear the screen
+
+  int16_t x = tft.width() / 2;                   // Calculate the x coordinate of the center of the screen
+  int16_t y = tft.height() / 2;                  // Calculate the y coordinate of the center of the screen
+  tft.pushImage(x - 30, y - 50, 128, 128, Trex); // Draw the T-Rex image at the center of the screen
+
+  delay(3000);               // Wait for 3 seconds
+  tft.fillScreen(TFT_BLACK); // Clear the screen
+  return;
+}
+
+void scan4challange()
+{
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(3);
+  tft.setTextColor(TFT_WHITE);
+  // tft.setCursor(0, tft.getCursorY() + 10); // Move the cursor down
+  tft.setCursor(0, 0);
+  tft.println("\nScan\nchallange\ncard.....");
+}
+
+void displayX()
+{
+  tft.fillScreen(TFT_BLACK); // Clear the screen
+
+  tft.setTextDatum(MC_DATUM); // Set the datum to be the center of the screen
+  tft.setTextSize(10);        // Set the text size to 5
+  tft.setTextColor(TFT_RED);  // Set the text color to red
+
+  int16_t x = tft.width() / 2;  // Calculate the x coordinate of the center of the screen
+  int16_t y = tft.height() / 2; // Calculate the y coordinate of the center of the screen
+
+  tft.drawString("X", x, y); // Draw the 'X' at the center of the screen
+
+  delay(1000); // Wait for 3 seconds
+
+  tft.fillScreen(TFT_BLACK); // Clear the screen
+}
+
+void displayCircle()
+{
+  tft.fillScreen(TFT_BLACK); // Clear the screen
+
+  int16_t x = tft.width() / 2;  // Calculate the x coordinate of the center of the screen
+  int16_t y = tft.height() / 2; // Calculate the y coordinate of the center of the screen
+  int16_t radius = 50 / 2;      // Calculate the radius of the circle
+
+  tft.fillCircle(x, y, radius, TFT_GREEN); // Draw the green circle
+
+  delay(1000); // Wait for 3 seconds
+
+  tft.fillScreen(TFT_BLACK); // Clear the screen
+}
+
+void animateEyes()
+{
+  int16_t x = tft.width() / 2;       // Calculate the x coordinate of the center of the screen
+  int16_t y = tft.height() / 2 + 40; // Calculate the y coordinate of the center of the screen
+  int16_t radius = 50 / 2;           // Calculate the radius of the circle
+  int16_t pupilRadius = radius / 2;  // Calculate the radius of the pupil
+
+  // Draw the white circles
+  tft.fillCircle(x - radius - 10, y, radius, TFT_WHITE);
+  tft.fillCircle(x + radius + 10, y, radius, TFT_WHITE);
+
+  for (int i = -radius + pupilRadius + 2; i <= radius - pupilRadius - 2; i += 5)
+  {
+    // Clear the previous pupils
+    tft.fillCircle(x - radius - 10 + i - 5, y, pupilRadius, TFT_WHITE);
+    tft.fillCircle(x + radius + 10 + i - 5, y, pupilRadius, TFT_WHITE);
+
+    // Draw the new pupils
+    tft.fillCircle(x - radius - 10 + i, y, pupilRadius, TFT_BLACK);
+    tft.fillCircle(x + radius + 10 + i, y, pupilRadius, TFT_BLACK);
+
+    delay(200); // Wait for a short time
+  }
+}
+
+void displayErrorMessage(const std::string &message)
+{
+  tft.fillScreen(TFT_BLACK); // Clear the screen
+
+  tft.setTextSize(3);
+  tft.setTextColor(TFT_RED);
+  tft.setCursor(0, 0);
+  tft.println(("\nError:\n" + message).c_str());
+
+  delay(1000); // Wait for 3 seconds
+  return;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,18 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-#include "Trex.h"
+// #include "Trex.h"
 #include "arduino_secrets.h"
 #include <HTTPClient.h>
 #include <WiFi.h>
+#include "displayFunctions.h"
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 char serverAddress[] = "https://gameapi-2e9bb6e38339.herokuapp.com/api/v1/resources"; // server address
 int port = 5000;                                                                      // server port
 
-TFT_eSPI tft; // Create an instance of the TFT class
+//TFT_eSPI tft; // Create an instance of the TFT class
 
 HardwareSerial mySerial(1); // Use the second hardware serial port
 
@@ -45,19 +46,6 @@ void noButtonPressed()
 const int yesButtonPin = 35; // Button1 on the TTGO T-Display
 const int noButtonPin = 0;   // Button2 on the TTGO T-Display
 
-void displayErrorMessage(const std::string &message)
-{
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-
-  tft.setTextSize(3);
-  tft.setTextColor(TFT_RED);
-  tft.setCursor(0, 0);
-  tft.println(("\nError:\n" + message).c_str());
-
-  delay(1000); // Wait for 3 seconds
-  return;
-}
-
 void connectToNetwork()
 {
   WiFi.begin(ssid, pass); // Connect to the network
@@ -76,87 +64,6 @@ void connectToNetwork()
   delay(2000); // Wait for 1 second
   // getPlayerData();
   delay(10000); // Wait for 10 seconds before next request
-}
-
-void displayTrex()
-{
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-
-  int16_t x = tft.width() / 2;                   // Calculate the x coordinate of the center of the screen
-  int16_t y = tft.height() / 2;                  // Calculate the y coordinate of the center of the screen
-  tft.pushImage(x - 30, y - 50, 128, 128, Trex); // Draw the T-Rex image at the center of the screen
-
-  delay(3000);               // Wait for 3 seconds
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-  return;
-}
-
-void scan4challange()
-{
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextSize(3);
-  tft.setTextColor(TFT_WHITE);
-  // tft.setCursor(0, tft.getCursorY() + 10); // Move the cursor down
-  tft.setCursor(0, 0);
-  tft.println("\nScan\nchallange\ncard.....");
-}
-
-void displayX()
-{
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-
-  tft.setTextDatum(MC_DATUM); // Set the datum to be the center of the screen
-  tft.setTextSize(10);        // Set the text size to 5
-  tft.setTextColor(TFT_RED);  // Set the text color to red
-
-  int16_t x = tft.width() / 2;  // Calculate the x coordinate of the center of the screen
-  int16_t y = tft.height() / 2; // Calculate the y coordinate of the center of the screen
-
-  tft.drawString("X", x, y); // Draw the 'X' at the center of the screen
-
-  delay(1000); // Wait for 3 seconds
-
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-}
-
-void displayCircle()
-{
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-
-  int16_t x = tft.width() / 2;  // Calculate the x coordinate of the center of the screen
-  int16_t y = tft.height() / 2; // Calculate the y coordinate of the center of the screen
-  int16_t radius = 50 / 2;      // Calculate the radius of the circle
-
-  tft.fillCircle(x, y, radius, TFT_GREEN); // Draw the green circle
-
-  delay(1000); // Wait for 3 seconds
-
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-}
-
-void animateEyes()
-{
-  int16_t x = tft.width() / 2;       // Calculate the x coordinate of the center of the screen
-  int16_t y = tft.height() / 2 + 40; // Calculate the y coordinate of the center of the screen
-  int16_t radius = 50 / 2;           // Calculate the radius of the circle
-  int16_t pupilRadius = radius / 2;  // Calculate the radius of the pupil
-
-  // Draw the white circles
-  tft.fillCircle(x - radius - 10, y, radius, TFT_WHITE);
-  tft.fillCircle(x + radius + 10, y, radius, TFT_WHITE);
-
-  for (int i = -radius + pupilRadius + 2; i <= radius - pupilRadius - 2; i += 5)
-  {
-    // Clear the previous pupils
-    tft.fillCircle(x - radius - 10 + i - 5, y, pupilRadius, TFT_WHITE);
-    tft.fillCircle(x + radius + 10 + i - 5, y, pupilRadius, TFT_WHITE);
-
-    // Draw the new pupils
-    tft.fillCircle(x - radius - 10 + i, y, pupilRadius, TFT_BLACK);
-    tft.fillCircle(x + radius + 10 + i, y, pupilRadius, TFT_BLACK);
-
-    delay(200); // Wait for a short time
-  }
 }
 
 std::pair<std::string, int> extractWordAndNumberString(const std::string &str)
