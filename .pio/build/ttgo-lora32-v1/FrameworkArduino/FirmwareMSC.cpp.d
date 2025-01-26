777fa4a8afd4250a33153336b979b896c16d5e7e
lukeC01e5 <colesluke@gmial.com>
2025-01-26 22:30:45 +1300

    Initial commit

--- .pio/build/ttgo-lora32-v1/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/ttgo-lora32-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/OEM/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/OEM/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/OEM/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/OEM/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/ttgo-lora32-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/ttgo-lora32-v1/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/OEM/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/OEM/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
